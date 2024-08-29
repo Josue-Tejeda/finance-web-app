bfccf28f5840d58e82853fb85c84e45b46df7e72
JoxueTe <josuetejeda02@gmail.com>
2024-08-29 11:26:55 -0500

    feat: category filters with tests

--- tracker/filters.py
+++ tracker/filters.py
@@ -1,6 +1,7 @@
 import django_filters
+from django import forms
 from tracker.data import TRANSACTION_TYPE_CHOICES
-from tracker.models import Transaction
+from tracker.models import Transaction, Category
 
 
 class TransactionFilter(django_filters.FilterSet):
@@ -11,6 +12,27 @@ class TransactionFilter(django_filters.FilterSet):
         empty_label='Any'
     )
     
+    start_date = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='gte',
+        label='Date From',
+        widget=forms.DateInput(attrs={'type':'date'})
+    )
+    
+    end_date = django_filters.DateFilter(
+        field_name='date',
+        lookup_expr='lte',
+        label='Date To',
+        widget=forms.DateInput(attrs={'type':'date'})
+    )
+    
+    category = django_filters.ModelMultipleChoiceFilter(
+        queryset=Category.objects.all(),
+        label='Categories',
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'daisy-checkbox-accent'}),
+    )
+    
     class Meta:
         model = Transaction
-        fields = ('transaction_type',)
+        fields = ('transaction_type', 'start_date', 'end_date', 'category')
+        
--- tracker/tests/test_views.py
+++ tracker/tests/test_views.py
@@ -1,5 +1,9 @@
+from datetime import  timedelta
+from django.utils import timezone
 import pytest
 from django.urls import reverse
+
+from tracker.models import Category
 
 
 @pytest.mark.django_db
@@ -15,3 +19,75 @@ def test_total_values_appearing_on_list_page(user_transactions, client):
     assert response.context['total_income'] == income_total
     assert response.context['total_expenses'] == expense_total
     assert response.context['net_income'] == net
+    
+@pytest.mark.django_db
+def test_transaction_type_filter(user_transactions, client):
+    user = user_transactions[0].user
+    client.force_login(user)
+    
+    #income test
+    GET_params = {'transaction_type': 'income'}
+    response = client.get(reverse('transactions-list'), GET_params)
+    
+    qs = response.context['filter'].qs
+    
+    for transaction in qs:
+        assert transaction.type == 'income'
+        
+    #expense test
+    GET_params = {'transaction_type': 'expense'}
+    response = client.get(reverse('transactions-list'), GET_params)
+    
+    qs = response.context['filter'].qs
+    
+    for transaction in qs:
+        assert transaction.type == 'expense'
+
+
+@pytest.mark.django_db
+def test_start_date_filter(user_transactions, client):
+    user = user_transactions[0].user
+    client.force_login(user)
+    
+    start_date = timezone.now() - timedelta(days=120)
+    
+    GET_params = {'start_date': start_date}
+    response = client.get(reverse('transactions-list'), GET_params)
+    
+    qs = response.context['filter'].qs
+    
+    for transaction in qs:
+        assert transaction.date >= start_date
+        
+
+@pytest.mark.django_db
+def test_end_date_filter(user_transactions, client):
+    user = user_transactions[0].user
+    client.force_login(user)
+    
+    end_date = timezone.now().date() - timedelta(days=20)
+    
+    GET_params = {'end_date': end_date}
+    response = client.get(reverse('transactions-list'), GET_params)
+    
+    qs = response.context['filter'].qs
+    
+    for transaction in qs:
+        assert transaction.date <= end_date
+        
+        
+@pytest.mark.django_db
+def test_category_filter(user_transactions, client):
+    user = user_transactions[0].user
+    client.force_login(user)
+    
+    category_ids = Category.objects.all()[:2].values_list('id', flat=True)
+    GET_params = {'category': category_ids}
+    
+    response = client.get(reverse('transactions-list'), GET_params)
+    
+    qs = response.context['filter'].qs
+    
+    for transaction in qs:
+        assert transaction.category.id in category_ids
+    
